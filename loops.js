1c5a1033d9e467034d586fa3380a755bdf3efd19
Rayshawn Woolard <r.woolard456@gmail.com>
2018-11-11 13:30:11 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,9 @@
+function forLoop(array) {
+  for(i = 1, i < 25, i++);
+  if (i === 1) {
+    return ("I am 1 strange loop")
+  } else {
+    return (`I am ${1} strange loop`)
+  }
+  return array;
+}
